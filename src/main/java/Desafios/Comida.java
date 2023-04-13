353e01edcb2678987d4c06199df7d37057fa9fc8
victoriarspaiva <victoriardspaiva@recife.pe.senac.br>
2023-04-12 21:05:28 -0300

    aula dia 12/04

---
+++ src/main/java/Desafios/Comida.java
@@ -0,0 +1,12 @@
+package Desafios;
+
+public class Comida {
+
+    String nome;
+    double peso;
+
+    Comida(String nome, double peso){
+        this.nome = nome;
+        this.peso = peso;
+    }
+}
--- src/main/java/classe/Data.java
+++ src/main/java/classe/Data.java
@@ -6,15 +6,16 @@ public class Data {
     int ano;
 
     Data(int dia, int mes, int ano) {
-        this.dia = dia;
+        this.dia = dia; // para evitar conflito entre o nomes dos parametro e das variaveis de intancia
         this.mes = mes;
         this.ano = ano;
     }
 
     Data() {
-        dia = 1;
-        mes = 1;
-        ano = 1970;
+        this(1,1,1970); //Data default
+//        dia = 1;
+//        mes = 1;
+//        ano = 1970;
     }
 
     String obterDataFormatada() {
