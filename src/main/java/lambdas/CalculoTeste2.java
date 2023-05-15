426997cb341e39e144dd6e246d5193260c0abe2b
Victoria Paiva <victoriardspaiva@recife.pe.senac.br>
2023-05-15 09:16:09 -0300

    Interface and Class Abstract

---
+++ src/main/java/lambdas/CalculoTeste2.java
@@ -0,0 +1,10 @@
+package lambdas;
+
+public class CalculoTeste2 {
+    public static void main(String[] args) {
+        
+        Calculo soma = (x, y) -> {
+            return x + y;
+        };
+    }
+}
